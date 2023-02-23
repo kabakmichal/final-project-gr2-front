db3188ad63ef9921c0ceacb045e79899fe21c8ab
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-02-23 19:10:44 +0100

    style-add-button

--- src/Components/AddButton/index.jsx
+++ src/Components/AddButton/index.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "./AddButton.module.css";
 import { QuestOrChallenge } from "../QuestOrChallenge";
 
+import Plus from "./plus.svg";
+
 export const AddButton = () => {
   const [components, setComponents] = useState([]);
 
@@ -12,11 +14,11 @@ export const AddButton = () => {
   return (
     <>
       <div className="addBtn">
+        <div className={styles.blur}></div>
         <button type="button" className={styles.btn} onClick={addComponent}>
-          +
+          <img src={Plus}></img>
         </button>
       </div>
-
       <div className="container">{components}</div>
     </>
   );
