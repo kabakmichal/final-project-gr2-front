5440b97f0c8ecdc0cf14bcfb751276d93fd37d8b
Michalinius <michalinius@gmail.com>
2023-02-23 20:18:50 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Tile from './components/Tile'
+import Dashboard from './pages/Dashboard.js'
+import '../node_modules/modern-normalize/modern-normalize.css';
 
 
 function App() {
   return (
-    <Tile />
+    <Dashboard />
   );
 }
 
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -5,20 +5,22 @@ const Tile = (props) => {
 
     const difficultyLevel = "tile_hard__9SxxV"
 
+    console.log(props)
+
     return (
         <>
             <div className={styles.tile}>
                 <div className={styles.first_row}>
                     <div className={styles.difficulty}>
-                        <span className={`${styles.dot} ${difficultyLevel}`}></span>
-                        <p className={styles.difficulty_level} >Hard</p>
+                        <span className={`${styles.dot} ${props.difficultyLevel}`}></span>
+                        <p className={styles.difficulty_level} >{props.difficultyLevel}</p>
                     </div>
                     <div className={styles.star}>Star</div>
                 </div>
-                <div className={styles.tile_title}>Name of Tiles</div>
-                <div className={styles.tile_date}> 12/09/1996</div>
+                <div className={styles.tile_title}>{props.title}</div>
+                <div className={styles.tile_date}> {props.date}</div>
                 <div className={styles.bottom_row}>
-                    <span className={styles.category_name}>HEALTH </span>
+                    <span className={styles.category_name}>{props.type} </span>
                 </div>
 
             </div>
