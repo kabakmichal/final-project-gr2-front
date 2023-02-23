add767c4fd9bb17f9d256136e79555f5cd4c440c
Tomasz Skolimo <skolim10@gmail.com>
2023-02-23 14:16:05 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./Pages/Home/Home.js";
 import AuthModal from "./Components/AuthModal/AuthModal";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Dashboard />
     </div>
   );
 }
