35d8d60b89a29453dc0e9769ae5ba85ec227f2b0
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2023-02-28 20:12:07 +0100

    Update

--- src/Components/QuestTile/QuestTile.js
+++ src/Components/QuestTile/QuestTile.js
@@ -1,4 +1,5 @@
 import React from "react"
+import DateTimePicker from "../DateTimePicker/DateTimePicker";
 import DifficultSelect from "../DifficultSelect/DifficultSelect";
 import styles from "./questTile.module.css"
 import { ReactComponent as Star } from "./star.svg"
@@ -22,7 +23,8 @@ const QuestTile = (props) => {
                 <div className={styles.tile_title}>
                     <p className={styles.tile_title_text}>{props.title}</p>
                 </div>
-                <div className={styles.tile_date}> {props.date}</div>
+                <DateTimePicker/>
+                {/* <div className={styles.tile_date}> {props.date}</div> */}
                 <div className={styles.bottom_container}>
                     <div className={`${styles.category} ${styles[props.type]}`}>
                         <span className={styles.category_name}>{props.type.toUpperCase()} </span>
