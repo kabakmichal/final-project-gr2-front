d668049e920f2bba1e1a3a88981678cddeb54d74
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-03-01 16:38:09 +0100

    working add button

--- src/Components/ListOfTiles/index.jsx
+++ src/Components/ListOfTiles/index.jsx
@@ -1,6 +1,6 @@
 import { AddButton } from "../../Components/AddButton";
 import React, { useState } from "react";
-import { Tile } from "../Tile/Tile";
+import { QuestTile } from "../QuestTile/QuestTile.jsx";
 import styles from "./ListOfTiles.module.css";
 
 export const ListOfTiles = () => {
@@ -8,7 +8,6 @@ export const ListOfTiles = () => {
 
   const addObject = () => {
     const newObject = {
-      hardness: "normal",
       title: "New quest",
       date: "Choose date",
       type: "Job",
@@ -22,12 +21,7 @@ export const ListOfTiles = () => {
       <ul className={styles.list_items}>
         {objects.map((obj) => (
           <li key={obj.id} className={styles.list_item}>
-            <Tile
-              difficultyLevel={obj.hardness}
-              title={obj.title}
-              date={obj.date}
-              type={obj.type}
-            />
+            <QuestTile title={obj.title} date={obj.date} type={obj.type} />
           </li>
         ))}
       </ul>
