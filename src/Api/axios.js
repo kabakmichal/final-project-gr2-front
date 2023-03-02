5a2d922e1909c412ce9b3e8ca5386f2d3e18d232
Tomasz Skolimo <skolim10@gmail.com>
2023-03-02 23:51:58 +0100

    rest connection changed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3500",
+  // baseURL: "http://localhost:3500",
+  baseURL: "  https://polite-churros-7ddf81.netlify.app/.netlify/functions",
 });
