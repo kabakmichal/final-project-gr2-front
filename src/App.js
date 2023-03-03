0b84a287ac1a32ac8dcaed1ff01a8785d14ff110
Tomasz Skolimo <skolim10@gmail.com>
2023-03-03 13:54:51 +0100

    dashboard only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
-import Home from "./Pages/Home/Home";
-import { PrivateRoute } from "./Components/PrivateRoute";
+// import Home from "./Pages/Home/Home";
+// import { PrivateRoute } from "./Components/PrivateRoute";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     // <Router>
-    <Routes>
-      <Route index path="/" element={<Home />} />
-      <Route
-        path="/dashboard"
-        element={<PrivateRoute redirectTo="/" component={<Dashboard />} />}
-      />
-      <Route path="*" element={<h1>There's nothing here: 404!</h1>} />
-    </Routes>
+    // <Routes>
+    //   <Route index path="/" element={<Home />} />
+    //   <Route
+    //     path="/dashboard"
+    //     element={<PrivateRoute redirectTo="/" component={<Dashboard />} />}
+    //   />
+    //   <Route path="*" element={<h1>There's nothing here: 404!</h1>} />
+    // </Routes>
     // </Router>
+    <Dashboard />
   );
 }
 
