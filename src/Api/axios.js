e1091940b2561ba45945198a39f754bfcc225d27
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-03-06 15:36:21 +0100

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3500",
-  // baseURL: "https://polite-churros-7ddf81.netlify.app/.netlify/functions",
-=======
   // baseURL: "http://localhost:3500",
   baseURL: "  https://polite-churros-7ddf81.netlify.app/.netlify/functions",
->>>>>>> dev
 });
