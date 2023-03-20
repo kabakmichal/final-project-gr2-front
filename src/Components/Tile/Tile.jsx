5a8d01c240bdc8456c19cbe15a3aab27ae0a1ab3
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-03-20 20:49:15 +0100

    changing single tile to edited without props

--- src/Components/Tile/Tile.jsx
+++ src/Components/Tile/Tile.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
-// import CategorySelect from "../CategorySelect/CategorySelect";
-// import DateTimePicker from "../DateTimePicker/DateTimePicker";
-// import DifficultSelect from "../DifficultSelect/DifficultSelect";
-// import styles from "./Tile.module.css";
-// import { ReactComponent as Star } from "./star.svg";
-// import { ReactComponent as Cup } from "./cup.svg";
 import ConfirmedTile from "../ConfirmedTile/ConfirmedTile";
 import { EditedTile } from "../EditedTile";
 
 export const Tile = (props) => {
   const [edit, setEdit] = useState(false);
-  // const handleStateChange = (edit) => {
-  //   setEdit(!edit);
-  // };
-  console.log(edit);
-  return (
+
+  const test = {
+    title: "title",
+    category: "learning",
+    data: "today",
+  };
+
+  const saveCurrentToDo = () => {
+    const testLocal = JSON.stringify(test);
+    localStorage.setItem("currentToDo", testLocal);
+  };
+
+  const handleEdit = () => {
+    setEdit();
+    // saveCurrentToDo();
+  };
+
+  return !edit ? (
     <ConfirmedTile
       id={props.id}
       title={props.title}
@@ -22,56 +29,20 @@ export const Tile = (props) => {
       type={props.type}
       difficultyLevel={props.difficultyLevel}
       category={props.category}
-      // handleStateChange={handleStateChange}
+      edit={edit}
+      // setEdit={handleEdit}
+      setEdit={setEdit}
+    />
+  ) : (
+    <EditedTile
+      id={props.id}
+      title={props.title}
+      date={props.date}
+      type={props.type}
+      difficultyLevel={props.difficultyLevel}
+      category={props.category}
       edit={edit}
       setEdit={setEdit}
     />
-    // <>
-    //   <div
-    //     className={
-    //       props.isQuest ? styles.tile_questbg : styles.tile_challengebg
-    //     }
-    //   >
-    //     <div className={styles.top_container}>
-    //       {/* <DifficultSelect /> */}
-    //       <div className={styles.difficulty}>
-    //         <span
-    //           className={`${styles.dot} ${styles[props.difficultyLevel]}`}
-    //         ></span>
-    //         <p className={styles.difficulty_level}>
-    //           {props.difficultyLevel.charAt(0).toUpperCase() +
-    //             props.difficultyLevel.slice(1)}
-    //         </p>
-    //       </div>
-    //       <div className={styles.picture}>
-    //         {props.isQuest ? <Star /> : <Cup />}
-    //       </div>
-    //     </div>
-    //     {props.isQuest ? (
-    //       <>
-    //         <div className={styles.tile_title}>
-    //           <p className={styles.tile_title_text}>{props.title}</p>
-    //         </div>{" "}
-    //       </>
-    //     ) : (
-    //       <>
-    //         <p className={styles.tile_sign}>CHALLENGE</p>
-    //         <div className={styles.tile_title_chl}>
-    //           <p className={styles.tile_title_text_chl}>{props.title}</p>
-    //         </div>{" "}
-    //       </>
-    //     )}
-    //     {/* <DateTimePicker /> */}
-    //     <div className={styles.tile_date}> {props.date}</div>
-    //     <div className={styles.bottom_container}>
-    //       {/* <CategorySelect /> */}
-    //       <div className={`${styles.category} ${styles[props.type]}`}>
-    //         <span className={styles.category_name}>
-    //           {props.type.toUpperCase()}{" "}
-    //         </span>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
   );
 };
